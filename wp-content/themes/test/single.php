22f02b0f46dbd9c4e0d50b61bf9f0ca7492903e0
boxgates-dung <dung.ky@vhgroup.io>
2023-12-15 17:41:33 +0700

    .

---
+++ wp-content/themes/test/single.php
@@ -0,0 +1,20 @@
+<div id="product-tabsss">
+  <ul class="product-tabs d-none d-md-flex flex-wrap w-100">
+    <li class="active"><a class="tablink" href="#ptabDesc">PRODUCT DETAILS</a></li>
+    <li class=""><a class="tablink" href="#ptabReview">REVIEWS</a></li>
+    <li class=""><a class="tablink" href="#ptabsize">SIZE &amp; FIT</a></li>
+    <li class=""><a class="tablink" href="#tabca4b9cc3-1de5-4957-9152-20d832cd0b41">SHIPPING &amp; RETURNS</a></li>
+    <li class=""><a class="tablink" href="#tab9946dcbc-20e7-49f8-b126-4356456b2dab">CUSTOM TAB</a></li>
+  </ul>
+
+  <div class="tab-container">
+    <h3 class="acor d-md-none"><a class="tablink" href="#ptabDesc">PRODUCT DETAILS<i class="at at-angle-down-r" aria-hidden="true"></i></a></h3>
+    <div id="ptabDesc" class="tab-content">
+
+    </div>
+    <h3 class="acor d-md-none active"><a class="tablink" href="#ptabReview">REVIEWS<i class="at at-angle-down-r" aria-hidden="true"></i></a></h3>
+    <div id="ptabReview" class="tab-content">
+    </div>
+
+  </div>
+</div>
